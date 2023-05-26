4cdd469c619f4ec6acf0d461073e23973630172c
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-05-26 13:56:50 +0200

    modify store

--- src/data/store.js
+++ src/data/store.js
@@ -4,7 +4,7 @@ export const store = reactive({
     ApiKey : '823176dcb211bbda8a3dead13880afe3',
     ApiUrl: 'https://api.themoviedb.org/3/search/',
     Movies: [],
-    Series : [],
+    SeriesTv : [],
     SearchInput: '',
     ApiImgUrl: 'https://image.tmdb.org/t/p/w500',
 });
