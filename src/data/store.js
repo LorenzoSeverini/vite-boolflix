4bc2bff1689af0b1ffce0d69af10701e9256938d
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-05-26 13:37:14 +0200

    add store

---
+++ src/data/store.js
@@ -0,0 +1,10 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    ApiKey : '823176dcb211bbda8a3dead13880afe3',
+    ApiUrl: 'https://api.themoviedb.org/3/search/',
+    Movies: [],
+    Series : [],
+    SearchInput: '',
+    ApiImgUrl: 'https://image.tmdb.org/t/p/w500',
+});
