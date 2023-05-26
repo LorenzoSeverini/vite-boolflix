9117fcb045e1c123c7ee0f4e04081f258e71bac2
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-05-26 13:36:35 +0200

    add function to search movies and S TV

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,25 +1,64 @@
 <!-- Script -->
 <script>
+import  {store} from '../data/store.js';
+import axios from 'axios';
+// provider vue 
+
+
 
   export default  {
     name: 'AppHeader',
-    
-    props: {
-      msg: String
-    },
 
     data() {
       return {
-        // 
+        store,
+        title : 'Netflix',      
+        searchInput : '',  
       }
     },
+
+    methods : {
+
+      search() {
+        console.log('search', this.searchInput);
+
+        // *********
+        // Movies 
+        axios.get(`${this.store.ApiUrl}movie?api_key=${this.store.ApiKey}&query=${this.searchInput}`)
+        .then((response) => {
+          const result = response.data.results;
+          console.log('Movies result :', result);
+        })
+        .catch((error) => {
+          console.log('Sorry FBI open up', error);
+        })
+
+        // *********
+        // Series Tv
+        axios.get(`${this.store.ApiUrl}tv?api_key=${this.store.ApiKey}&query=${this.searchInput}`)
+        .then((response) => {
+          const result = response.data.results;
+          console.log('Series Tv result :', result);
+        })
+        .catch((error) => {
+          console.log('Sorry FBI open up', error);
+        })
+      },
+    }
   }
 
 </script>
 
 <!-- Template -->
 <template>
-  <h1>{{ msg }}</h1>
+
+  <h1>{{ title }}</h1>
+  <!-- search bar e pulsante per visualizzare la lista dei film -->
+  <div class="search-box">
+    <input v-model="searchInput" class="search-bar" type="text" placeholder="Search a Movie" @keyup.enter="search">
+    <button class="btn-search" @click="search">Search</button>
+  </div>
+
 </template>
 
 <!-- Style -->
@@ -36,4 +75,47 @@ h1 {
   padding: 0;
   text-align: center;
 }
+
+.search-box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2rem 0;
+  gap: 1rem;
+
+  .search-bar {
+    width: 50%;
+    padding: 0.5rem;
+    border: 1px solid $color-primary;
+    border-radius: 5px;
+    outline: none;
+    font-size: 1.5rem;
+    font-weight: 700;
+    color: $color-primary;
+    text-align: center;
+
+    &:focus {
+      border: 1px solid $color-secondary;
+    }
+  }
+
+  .btn-search {
+    padding: 0.5rem 1rem;
+    border: 1px solid $color-primary;
+    border-radius: 5px;
+    outline: none;
+    font-size: 1.5rem;
+    font-weight: 700;
+    color: $color-primary;
+    background-color: $color-secondary;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+      background-color: $color-primary;
+      color: $color-secondary;
+    }
+  }
+}
+
 </style>
