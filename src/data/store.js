943453feb001466f8c96918186a99af6c463de0d
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-05-26 15:39:43 +0200

    add API Img Url

--- src/data/store.js
+++ src/data/store.js
@@ -3,8 +3,8 @@ import { reactive } from "vue";
 export const store = reactive({
     ApiKey : '823176dcb211bbda8a3dead13880afe3',
     ApiUrl: 'https://api.themoviedb.org/3/search/',
+    ApiImgUrl: 'https://image.tmdb.org/t/p/w185/',
     Movies: [],
     SeriesTv : [],
     SearchInput: '',
-    ApiImgUrl: 'https://image.tmdb.org/t/p/w500',
 });
