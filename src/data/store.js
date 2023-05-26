c025a2d56b2f83cb3001b33c296a9f00e9d0acbc
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-05-26 18:23:57 +0200

    change API img url

--- src/data/store.js
+++ src/data/store.js
@@ -3,7 +3,7 @@ import { reactive } from "vue";
 export const store = reactive({
     ApiKey : '823176dcb211bbda8a3dead13880afe3',
     ApiUrl: 'https://api.themoviedb.org/3/search/',
-    ApiImgUrl: 'https://image.tmdb.org/t/p/w185/',
+    ApiImgUrl: 'https://image.tmdb.org/t/p/',
     Movies: [],
     SeriesTv : [],
     SearchInput: '',
