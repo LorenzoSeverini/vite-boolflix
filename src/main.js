d037f147921aaa5431d12f4669b48bd3722e629f
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-05-29 16:26:49 +0200

    import font awesome library

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './styles/generic.scss'
 import App from './App.vue'
-
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 createApp(App).mount('#app')
