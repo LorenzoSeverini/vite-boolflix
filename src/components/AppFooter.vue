761b8b10ca5bbaf27474297eb5f6ea8d414f7b50
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-05-29 16:27:02 +0200

    add content in footer section

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -10,7 +10,6 @@ import axios from 'axios'
         data () {
             return {
                 store,
-                Footer : 'Footerrrrr',
             }
         },
     }
@@ -19,7 +18,21 @@ import axios from 'axios'
 
 <!-- template -->
 <template>
-    <h1>{{ Footer }}</h1>
+
+    <div class="app-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <p class="text-center">
+                        <div>
+                            <a href="https://www.themoviedb.org/" target="_blank">Data provided by TMDb 🎦</a>
+                        </div>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <!-- style -->
@@ -28,13 +41,23 @@ import axios from 'axios'
 @use '../styles/partials/_variables' as *;
 @use '../styles/partials/_mixing' as *;
 
-h1 {
-    color: $color-primary;
-    font-size: 5rem;
-    font-weight: 700;
-    margin: 0;
-    padding: 0;
+.app-footer {
+    background-color: $color-secondary;
+    padding: 20px 0;
+    color: $color-white;
+    font-size: 14px;
+    font-weight: 300;
     text-align: center;
+
+    a {
+        color: $color-primary;
+        text-decoration: none;
+
+        &:hover {
+            text-decoration: underline;
+        }
+    }
 }
+
 
 </style>
